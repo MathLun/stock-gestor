db2c8da815bf5db251693ca442729dbe7987d601
mathlun <matheuslunadev@gmail.com>
2025-07-17 20:33:19 -0300

    change: mudando o parametros de roteamento para um roteamento mais amigavel, para o ambiente de produção

--- frontend/web/forgot-password/index.js
+++ frontend/web/forgot-password/index.js
@@ -1,6 +1,6 @@
 import { openLink } from "../features/open_link.mjs";
 
-const loginLink = openLink("/frontend/web/login")
+const loginLink = openLink("/login")
 document.getElementById("loginLink").href = loginLink
 
 document.getElementById('submit').addEventListener('submit', async (e) => {
--- frontend/web/index.js
+++ frontend/web/index.js
@@ -1,7 +1,7 @@
 import { openLink } from "./features/open_link.mjs"
 
-const dashboardLink = openLink('/frontend/web/dashboard')
+const dashboardLink = openLink('/dashboard')
 document.getElementById('dashboardLink').href = dashboardLink
 
-const loginLink = openLink("/frontend/web/login")
+const loginLink = openLink("/login")
 document.getElementById('loginLink').href = loginLink
