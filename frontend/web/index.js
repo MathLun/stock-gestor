7ce53816845b92b473882da337c2c2ea6095328a
mathlun <matheuslunadev@gmail.com>
2025-07-17 10:50:39 -0300

    feat: novamente testando a condição da porta da url em produção

--- frontend/web/index.js
+++ frontend/web/index.js
@@ -1,4 +1,4 @@
 
 const { protocol, hostname, port } = window.location
-const url = `${protocol}//${hostname}${(port == undefined && ':' + port) ?? undefined}`.replace(":", "")
+const url = `${protocol}//${hostname}${(port != undefined && ':' + port) ?? undefined}`.replace(":", "")
 console.log(url)
