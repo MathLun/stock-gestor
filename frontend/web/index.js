2f9152a4c1f150d795217c5aab5f09544d9ce4d7
mathlun <matheuslunadev@gmail.com>
2025-07-17 11:07:27 -0300

    feat: testar o link de login com a features openLink para vê se redireciona

--- frontend/web/index.js
+++ frontend/web/index.js
@@ -1,5 +1,11 @@
 
-const { protocol, hostname, port } = window.location
-const url = `${protocol}//${hostname}${(port == undefined && ':' + port) || undefined}`
-const url_handler = url.replace("undefined", "")
-console.log(url_handler)
+const openLink = (route) => {
+    const { protocol, hostname, port } = window.location
+    const url = `${protocol}//${hostname}${(port != undefined && ':' + port) || undefined}${route}/index.html`
+    const url_handler = url.replace("undefined", "")
+    return url_handler
+}
+
+const url = openLink("/frontend/web/login")
+
+document.getElementById('loginLink').href = url
