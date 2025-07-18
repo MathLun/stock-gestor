f7e8db661a0c0fddf9df147d64f10ac6aab0751e
mathlun <matheuslunadev@gmail.com>
2025-07-17 23:40:02 -0300

    add: adicionando o 'dashboardLink' ao console, para verificar como o link está se comportando em produção.

--- frontend/web/index.js
+++ frontend/web/index.js
@@ -1,8 +1,8 @@
 import { openLink } from "./features/open_link.mjs"
 
 const dashboardLink = openLink('/dashboard')
-document.getElementById('dashboardLink').href = dashboardLink
-
+//document.getElementById('dashboardLink').href = dashboardLink
+console.log(dashboardLink)
 
 const loginLink = openLink("/login")
 document.getElementById('loginLink').href = loginLink
