88263204d80a6060226f77b9567560fa3e35689f
mathlun <matheuslunadev@gmail.com>
2025-07-19 10:36:21 -0300

    feat: ainda testando o openLink em produção.

--- frontend/web/features/open_link.mjs
+++ frontend/web/features/open_link.mjs
@@ -1,6 +1,6 @@
 export const openLink = (route) => {
     const { protocol, hostname, port } = window.location
-    const url = `${protocol}//${hostname}${(port !== undefined && ':' + port + '/frontend/web') ?? undefined}${route}/index.html`
-    const url_handler = url.replace("true", "")
+    const url = `${protocol}//${hostname}${port ?  ':' + port + '/frontend/web' : undefined}${route}/index.html`
+    const url_handler = url.replace("undefined", "")
     return url_handler
 }
