91d73ece200b92b0b3a7f7e73836da050cd00d35
mathlun <matheuslunadev@gmail.com>
2025-07-19 10:25:51 -0300

    change: substituindo o return de openLink, de url para url_handler novamente.

--- frontend/web/features/open_link.mjs
+++ frontend/web/features/open_link.mjs
@@ -2,5 +2,5 @@ export const openLink = (route) => {
     const { protocol, hostname, port } = window.location
     const url = `${protocol}//${hostname}${(port !== undefined && ':' + port + '/frontend/web') ?? undefined}${route}/index.html`
     const url_handler = url.replace("true", "")
-    return url
+    return url_handler
 }
