f372b0d0a3d14a39198d764e299b22f0f4302841
mathlun <matheuslunadev@gmail.com>
2025-07-19 10:07:21 -0300

    change: em openLink, mudando o != para !==, na logica para verificar se a port é undefined ou não.

--- frontend/web/index.js
+++ frontend/web/index.js
@@ -2,7 +2,8 @@ import { openLink } from "./features/open_link.mjs"
 
 const dashboardLink = openLink('/dashboard')
 document.getElementById('dashboardLink').href = dashboardLink
-
+console.log(dashboardLink)
 
 const loginLink = openLink("/login")
 document.getElementById('loginLink').href = loginLink
+
